8f1de4d0476742d956c8334668aefc5097967066
mahanfr <mahan.farzaneh@gmail.com>
2026-08-16 13:01:44 +0330

    Setting up pub/sec keys and wallet address

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use ed25519_dalek::{Signer, SigningKey, Verifier, VerifyingKey};
+use rand::{TryRng, rngs::SysRng};
+
+type PrivateKey = SigningKey;
+type PublicKey = VerifyingKey;
+
+fn generate_key_pair() -> Result<(PrivateKey, PublicKey)> {
+    let mut rng = SysRng;
+    let mut secret = [0u8; 32];
+    rng.try_fill_bytes(&mut secret)?;
+    let private_key = SigningKey::from_bytes(&secret);
+    let public_key : VerifyingKey = private_key.verifying_key();
+
+    Ok((private_key, public_key))
+}
+
+fn generate_address(pub_key: &PublicKey) -> String {
+    blake3::hash(pub_key.as_bytes()).to_string()
+}
+
+fn main() -> Result<()> {
+    let msg = b"Hello World";
+    let (priv_key, pub_key) = generate_key_pair()?;
+    let signature = priv_key.sign(msg);
+    if pub_key.verify(msg, &signature).is_ok() {
+        println!("The signature is valid");
+    }
+    println!("Wallet Address = {}",generate_address(&pub_key));
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn generate_valid_signature() {
+        let msg = b"Hello World";
+        let (priv_key, pub_key) = generate_key_pair().unwrap();
+        let signature = priv_key.sign(msg);
+        assert!(pub_key.verify(msg, &signature).is_ok());
+    }
+}
